379afc813c3328a8ed7fc86821c66536ea02007a
Matt Riemer <matthew@agilestyle.com>
2016-09-08 10:51:46 -0600

    Improve searching of orders

--- app/models/effective/datatables/qb_syncs.rb
+++ app/models/effective/datatables/qb_syncs.rb
@@ -12,18 +12,33 @@ if defined?(EffectiveDatatables)
             qb_ticket.qb_requests.length
           end
 
-          array_column :orders do |qb_ticket|
+          table_column :orders, sortable: false, as: :obfuscated_id do |qb_ticket|
             qb_ticket.qb_requests.select { |qb_request| qb_request.order.present? }
               .map { |qb_request| link_to "##{qb_request.order.to_param}", effective_orders.admin_order_path(qb_request.order) }
               .join('<br>').html_safe
           end
 
-          table_column :actions, sortable: false, filter: false, partial: 'admin/qb_syncs/actions', partial_local: :qb_sync
+          actions_column partial: 'admin/qb_syncs/actions', partial_local: :qb_sync
         end
 
         def collection
           Effective::QbTicket.includes(qb_requests: :order)
         end
+
+        def search_column(collection, table_column, search_term, sql_column)
+          if table_column[:name] == 'orders'
+            order = Effective::Order.where(id: search_term).first
+
+            if order.present?
+              collection.where(id: Effective::QbRequest.where(order_id: order.id).pluck(:qb_ticket_id))
+            else
+              collection.none
+            end
+          else
+            super
+          end
+        end
+
       end
     end
   end
