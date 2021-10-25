c838894e141c64980f8443b873fb41b54ac3fdd9
Matt Riemer <matthew@codeandeffect.com>
2021-10-25 10:27:07 -0600

    better display of orders on qb sync datatable

--- app/datatables/effective_qb_sync_datatable.rb
+++ app/datatables/effective_qb_sync_datatable.rb
@@ -9,20 +9,7 @@ class EffectiveQbSyncDatatable < Effective::Datatable
       qb_ticket.qb_requests.length
     end
 
-    val :orders, sort: false, as: :obfuscated_id do |qb_ticket|
-      qb_ticket.qb_requests.select { |qb_request| qb_request.order.present? }
-    end.format do |requests|
-      requests.map { |qb_request| link_to "##{qb_request.order.to_param}", effective_orders.admin_order_path(qb_request.order) }
-      .join('<br>').html_safe
-    end.search do |collection, term, column, sql_column|
-      order = Effective::Order.where(id: search_term).first
-
-      if order.present?
-        collection.where(id: Effective::QbRequest.where(order_id: order.id).pluck(:qb_ticket_id))
-      else
-        collection.none
-      end
-    end
+    col :orders
 
     actions_col do |qb_ticket|
       dropdown_link_to 'Show', effective_qb_sync.admin_qb_sync_path(qb_ticket)
@@ -31,7 +18,7 @@ class EffectiveQbSyncDatatable < Effective::Datatable
   end
 
   collection do
-    Effective::QbTicket.deep.all
+    Effective::QbTicket.deep.includes(:orders).all
   end
 
 end
